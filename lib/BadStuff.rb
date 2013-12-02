dbafeb08d1049a84021d9a6c12e3a05e8adad8b7
Luciabs <luciabs_012@hotmail.com>
2013-12-02 23:54:18 +0100

    Update BadStuff.rb

--- lib/BadStuff.rb
+++ lib/BadStuff.rb
@@ -53,4 +53,38 @@ class BadStuff
   def to_s
     "BadStuf: #{@text} \n Levels: #{@levels}  \n Visible Treasures: #{@nVisibleTreasures} \n Hidden Treasures: #{@nHiddenTreasures} \n specific Hidden Treasures #{@specificHiddenTreasures} \n specific Visible Treasures #{@specificVisibleTreasures} \n"
   end
+    def is_empty
+    if(self.text && self.levels== 0 && self.nVisible == 0 && self.nHidden == 0)
+      return true
+    else 
+      return false
+    end
+  end
+  
+   def my_bad_stuff_is_dead
+     if( self.death == true)
+       return true
+     else 
+       return false
+     end
+     
+   end
+  
+   def substract_visible_treasure(t)
+     for i in self.sVisible.length
+       if(self.sVisible.get(i).equals(t))
+         self.sVisible.get(i).remove()
+       end
+     end
+     
+   end 
+   
+   def subtract_hidden_treasure(t)
+     for i in self.sVisible.length
+       if(self.sVisible.get(i).equals(t))
+         self.sVisible.get(i).remove()
+       end
+     end
+   end
+   
 end
